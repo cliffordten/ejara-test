ab7ef1f3dbcb60b2c3fc373b4a9be72e59771520
Teneng Clifford <cliffordteng5@gmail.com>
2022-04-01 05:58:02 +0100

    feat: added swagger ui doc

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FeesModule } from './fees/fees.module';
 
 @Module({
   imports: [FeesModule],
-  controllers: [AppController],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
--- src/fees/fees.controller.ts
+++ src/fees/fees.controller.ts
@@ -1,21 +1,41 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { ResultHistoryModel, ResultModel } from './fees.model';
 import { FeesService } from './fees.service';
+import { ApiParam, ApiTags, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('Fees Module')
 @Controller('fees')
 export class FeesController {
   constructor(private readonly feeService: FeesService) {}
 
+  @ApiResponse({
+    description: 'Get the avaerage fee for the latest block in blockcain',
+    status: 200,
+  })
   @Get('/latest')
   async getLatestBlockFee(): Promise<ResultModel> {
     return await this.feeService.computeReqResult();
   }
 
+  @ApiResponse({
+    description: 'Get the avaerage fee for the block in blockcain by hash',
+    status: 200,
+  })
   @Get('/requestHistory')
   getRequestHistory(): { history: ResultHistoryModel[] } {
     return { history: this.feeService.getResultHistory() };
   }
 
+  @ApiResponse({
+    description: 'Get History List of all queries that is stored in Memory',
+    status: 200,
+  })
+  @ApiParam({
+    name: 'block_number',
+    type: 'string',
+    description: 'Blockchain hash that identifies the block in the blockchain',
+    example: 'BLkuidhUBW2KvdjBUdr25Rga83RdQfCdv8T1pfXwRLWKveQ3msM',
+  })
   @Get(':block_number')
   async getBlockFee(
     @Param('block_number') blockNumber: string,
